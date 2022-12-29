b82eadbd10802a2fd013e7263ac3e86fa394d867
jhxxr <2471717907@qq.com>
2022-12-29 19:51:46 +0800

    更新题库

--- js/public.js
+++ js/public.js
@@ -1,3 +1,5 @@
 const JSONList = [
     { id: "202211", name: '测试题库，近代史', describe: '近代史纲要', file: "20221229183253.json" },
+    { id: "202201", name: '第三、第四章练习卷', describe: '近代史纲要', file: "2022jds34z.json" },
+    
 ]
