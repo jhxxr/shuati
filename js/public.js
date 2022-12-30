8b79903a94e788d9f51664c35f32028f50b33338
jhxxr <2471717907@qq.com>
2022-12-30 12:49:17 +0800

    更新题库

--- js/public.js
+++ js/public.js
@@ -2,5 +2,9 @@ const JSONList = [
     { id: "202211", name: '第56章练习卷', describe: '近代史纲要', file: "20221229183253.json" },
     { id: "202201", name: '第三、第四章练习卷', describe: '近代史纲要', file: "2022jds34z.json" },
     { id: "202201", name: '第一、第二章练习卷', describe: '近代史纲要', file: "2022jds12z.json" },
-    
+    { id: "202201", name: '第一章练习卷', describe: '近代史纲要', file: "第一章.json" },
+    { id: "202201", name: '第二章练习卷', describe: '近代史纲要', file: "第二章.json" },
+    { id: "202201", name: '第三章练习卷', describe: '近代史纲要', file: "第三章.json" },
+    { id: "202201", name: '第四章练习卷', describe: '近代史纲要', file: "第四章.json" },
+    { id: "202201", name: '第五章练习卷', describe: '近代史纲要', file: "第五章.json" },
 ]
